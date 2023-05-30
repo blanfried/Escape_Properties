cc19278875a1f2fda0ddb232ae9e8c53f69c50b9
Baer Lanfried <blanfried@yahoo.com>
2023-05-30 23:06:11 +0200

    aesthetic adjustments comme avant

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,3 +33,21 @@ rightArrow.addEventListener('click', slideRight);
 
 window.addEventListener('resize', adjustContainerSize);
 window.addEventListener('load', adjustContainerSize);
+
+    // JavaScript to control dropdown menu behavior
+    document.addEventListener("DOMContentLoaded", function(event) {
+      var dropdown = document.querySelector(".dropdown");
+      var dropdownContent = document.querySelector(".dropdown-content");
+
+      dropdown.addEventListener("click", function() {
+        dropdownContent.classList.toggle("show");
+      });
+
+      window.addEventListener("click", function(event) {
+        if (!event.target.matches(".dropbtn")) {
+          if (dropdownContent.classList.contains("show")) {
+            dropdownContent.classList.remove("show");
+          }
+        }
+      });
+    });
