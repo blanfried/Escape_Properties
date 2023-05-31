0bb08ff7f535210fb52936dcf048d8ddc0487a97
Baer Lanfried <blanfried@yahoo.com>
2023-05-31 12:00:57 +0200

    changed js for slider to include id as an arg so I can use multiple different sliders (made 10 total so far), uploaded all the pics for each property and all is functioning correctly

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,52 @@
-const sliderContainer = document.querySelector('.slider-container');
-const sliderWrapper = document.querySelector('.slider-wrapper');
-const sliderItems = document.querySelectorAll('.slider-item');
-const leftArrow = document.querySelector('.slider-arrow.left');
-const rightArrow = document.querySelector('.slider-arrow.right');
-let currentPosition = 0;
+function createSlider(id) {
+  const sliderContainer = document.querySelector(`#${id} .slider-container`);
+  const sliderWrapper = document.querySelector(`#${id} .slider-wrapper`);
+  const sliderItems = document.querySelectorAll(`#${id} .slider-item`);
+  const leftArrow = document.querySelector(`#${id} .slider-arrow.left`);
+  const rightArrow = document.querySelector(`#${id} .slider-arrow.right`);
+  let currentPosition = 0;
 
-function adjustContainerSize() {
-  const currentImage = sliderItems[currentPosition].querySelector('img');
-  sliderContainer.style.width = `${currentImage.offsetWidth}px`;
+  function adjustContainerSize() {
+    const currentImage = sliderItems[currentPosition].querySelector('img');
+    sliderContainer.style.width = `${currentImage.offsetWidth}px`;
+  }
+
+  function showCurrentSlide() {
+    sliderItems.forEach((item, index) => {
+      item.classList.toggle('active', index === currentPosition);
+    });
+  }
+
+  function slideLeft() {
+    currentPosition = (currentPosition - 1 + sliderItems.length) % sliderItems.length;
+    showCurrentSlide();
+    adjustContainerSize();
+  }
+
+  function slideRight() {
+    currentPosition = (currentPosition + 1) % sliderItems.length;
+    showCurrentSlide();
+    adjustContainerSize();
+  }
+
+  leftArrow.addEventListener('click', slideLeft);
+  rightArrow.addEventListener('click', slideRight);
+
+  window.addEventListener('resize', adjustContainerSize);
+  window.addEventListener('load', adjustContainerSize);
 }
 
-function showCurrentSlide() {
-  sliderItems.forEach((item, index) => {
-    item.classList.toggle('active', index === currentPosition);
-  });
-}
-
-function slideLeft() {
-  currentPosition = (currentPosition - 1 + sliderItems.length) % sliderItems.length;
-  showCurrentSlide();
-  adjustContainerSize();
-}
-
-function slideRight() {
-  currentPosition = (currentPosition + 1) % sliderItems.length;
-  showCurrentSlide();
-  adjustContainerSize();
-}
+createSlider('slider1');
+createSlider('slider2');
+createSlider('slider3');
+createSlider('slider4');
+createSlider('slider5');
+createSlider('slider6');
+createSlider('slider7');
+createSlider('slider8');
+createSlider('slider9');
+createSlider('slider10');
+// create more sliders as needed
 
 leftArrow.addEventListener('click', slideLeft);
 rightArrow.addEventListener('click', slideRight);
